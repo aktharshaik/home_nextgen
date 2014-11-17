1a593786941e3c13216c9daba24fa2f727687f37
kteza1 <k.teza1@gmail.com>
2014-11-17 10:46:54 +0530

    Basic communication between base and hub. Hub creates a new thread for each connection request

---
+++ android hub/app/src/main/java/com/example/ravitejareddy/home_nextgen/ServerReplyThread.java
@@ -0,0 +1,56 @@
+package com.example.ravitejareddy.home_nextgen;
+
+import android.net.LocalSocket;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.SocketException;
+
+/**
+ * Created by ravitejareddy on 15/11/14.
+ */
+public class ServerReplyThread extends Thread {
+    private Socket hostThreadSocket;
+    public static final String TAG = ServerReplyThread.class.getSimpleName();
+    int cnt;
+    String message = "";
+
+    ServerReplyThread(Socket socket, int c) {
+        hostThreadSocket = socket;
+        cnt = c;
+    }
+
+    public void run() {
+        OutputStream outputStream;
+        String msgReply = "Hello from Android, you are #" + cnt;
+
+        //message = "#" + cnt + " from " + hostThreadSocket.getInetAddress() + ":" + hostThreadSocket.getPort() + "\n";
+        Log.d(TAG, message);
+
+        try {
+            BufferedReader input = new BufferedReader(new InputStreamReader(hostThreadSocket.getInputStream()));
+            String string = "";
+            outputStream = hostThreadSocket.getOutputStream();
+            PrintStream printStream = new PrintStream(outputStream);
+            //printStream.print(msgReply);
+            while(true){
+                Log.d(TAG, "Waiting for input");
+                string = input.readLine();
+                Log.d(TAG, "Writing to client");
+                printStream.print(string+"\n");
+                Log.d(TAG, "Written");
+            }
+        } catch (SocketException e) {
+            Log.d(TAG, "SOMETHING WENT WRONG");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
