66327bced8d6ff5eee443e5c0acfdbf1f2c02cd9
RTR <k.teza1@gmail.com>
2015-01-21 01:00:27 +0530

    Better commenting and variable names

--- server/server.go
+++ server/server.go
@@ -21,7 +21,9 @@ func main() {
 	msgchan := make(chan string)
 	addchan := make(chan Client)
 	rmchan := make(chan net.Conn)
+	/* One 'handleMessages()' go routine */
 	go handleMessages(msgchan, addchan, rmchan)
+	/* a 'handleConnection()' go routine per each successful connection */
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
@@ -32,23 +34,30 @@ func main() {
 	}
 }
 
-func handleConnection(c net.Conn, msgchan chan<- string, addchan chan<- Client, rmchan chan<- net.Conn) {
+func handleConnection(conn net.Conn, msgchan chan<- string, addchan chan<- Client, rmchan chan<- net.Conn) {
+	/* New string channel to this client. Client is 'conn & ch' */
 	ch := make(chan string)
+	/* messages received by client will be sent to this 'msgs' channel */
 	msgs := make(chan string)
-	addchan <- Client{c, ch}
+
+	/* Since client is successfully connected, add this client to the addchan channel. handleMessage() will add this client to the map */
+	addchan <- Client{conn, ch}
+
+	/* go routine to read messages recived by this client and send it to 'msgs' channel */
 	go func() {
 		defer close(msgs)
-		bufc := bufio.NewReader(c)
-		c.Write([]byte("\033[1;30;41mWelcome to the fancy demo chat!\033[0m\r\nWhat is your nick? "))
-		nick, _, err := bufc.ReadLine()
+		buffcon := bufio.NewReader(conn)
+		conn.Write([]byte("\033[1;30;41mWelcome to the fancy demo chat!\033[0m\r\nWhat is your nick? "))
+		nick, _, err := buffcon.ReadLine()
 		if err != nil {
 			return
 		}
 		nickname := string(nick)
-		c.Write([]byte("Welcome, " + nickname + "!\r\n\r\n"))
+		conn.Write([]byte("Welcome, " + nickname + "!\r\n\r\n"))
 		msgs <- "New user " + nickname + " has joined the chat room."
+		/* Read the messages received by this client connection 'c'  and send it to 'msgs' channel */
 		for {
-			line, _, err := bufc.ReadLine()
+			line, _, err := buffcon.ReadLine()
 			if err != nil {
 				break
 			}
@@ -56,6 +65,8 @@ func handleConnection(c net.Conn, msgchan chan<- string, addchan chan<- Client, 
 		}
 		msgs <- "User " + nickname + " left the chat room."
 	}()
+
+	/* 1. Send from 'msgs' channel to 'msgchan' channel. 2. If there is any data in 'ch' channel, write it to client connection 'c' */
 LOOP:
 	for {
 		select {
@@ -64,30 +75,34 @@ LOOP:
 				break LOOP
 			}
 			msgchan <- msg
+		/* Any string in 'ch' channel, will be written back to client */
 		case msg := <-ch:
-			_, err := c.Write([]byte(msg))
+			_, err := conn.Write([]byte(msg))
 			if err != nil {
 				break LOOP
 			}
 		}
 	}
-	c.Close()
-	fmt.Printf("Connection from %v closed.\n", c.RemoteAddr())
-	rmchan <- c
+	conn.Close()
+	fmt.Printf("Connection from %v closed.\n", conn.RemoteAddr())
+	rmchan <- conn
 }
 
 func handleMessages(msgchan <-chan string, addchan <-chan Client, rmchan <-chan net.Conn) {
 	clients := make(map[net.Conn]chan<- string)
 	for {
 		select {
+		/* Any string in 'msgchan' channel, send to all the registered clients */
 		case msg := <-msgchan:
 			fmt.Printf("new message: %s\n", msg)
 			for _, ch := range clients {
 				go func(mch chan<- string) { mch <- "\033[1;33;40m" + msg + "\033[m\r\n" }(ch)
 			}
+		/* When new client is added by handleConnection(), add it to map */
 		case client := <-addchan:
 			fmt.Printf("New client: %v\n", client.conn)
 			clients[client.conn] = client.ch
+		/* When connection is closed by handleConnection(), remove it from the map */
 		case conn := <-rmchan:
 			fmt.Printf("Client disconnects: %v\n", conn)
 			delete(clients, conn)
